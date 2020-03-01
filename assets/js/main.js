3f576e53f7594eca6f056a7061dab5e6c629f5f0
squelsh84 <mrmjwalsh@gmail.com>
2020-03-01 21:25:18 +0000

    Fixed scrollspy issue and added more text to about me.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,4 @@
-$(document).ready(function () {
-  $('#mainCarousel').carousel({
-    interval: 4000
-  });
-
-  var clickEvent = false;
-  $('#mainCarousel').on('click', '.nav a', function () {
-    clickEvent = true;
-    $('.nav li').removeClass('active');
-    $(this).parent().addClass('active');
-  }).on('slid.bs.carousel', function (e) {
-    if (!clickEvent) {
-
-      var count = $('#indexmenu').children().length - 1;
-      var current = $('#indexmenu li.active');
-      current.removeClass('active').next().addClass('active');
-      var id = parseInt(current.next().data('slide-to'));
-      if (isNaN(id)) {
-        $('#indexmenu li').first().addClass('active');
-      }
-    }
-    clickEvent = false;
-  });
-});
-
-
+/*-- built in function for sections appearing on scroll --*/
 
 $(document).ready(function () {
   new WOW().init();
@@ -32,17 +7,7 @@ $(document).ready(function () {
 
 
 
-
-$(function () {
-  $('a[href*=#]').on('click', function (e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 500, 'linear');
-  });
-});
-
-
-
-/* Back to top btn */
+/*-- Back to top btn --*/
 
 let btn = $('#back-to-top');
 
@@ -61,12 +26,9 @@ btn.on('click', (e) => {
 
 
 
-/* function to refresh page when Modal close button is clicked */
+/*-- function to refresh page when Modal close button is clicked --*/
 
 function refreshPage() {
   window.location.reload();
 }
-
-
-
 
